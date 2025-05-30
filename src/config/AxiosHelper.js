62d9f94cbc01296c063ab42a57916d308ce62e9c
Dipak Kurmi <kurmideepak7@gmail.com>
2025-05-30 13:00:48 +0530

    baseUrl updated3

--- src/config/AxiosHelper.js
+++ src/config/AxiosHelper.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
-export const baseURL = "https://chatappbackend-production.up.railway.app/api/v1"
+export const baseURL = "https://chatappbackend-production.up.railway.app/api/v1";
+const API_BASE_URL = process.env.REACT_APP_API_URL + '/api/v1';
+
 export const httpClient = axios.create({
     baseURL: baseURL,
 });
