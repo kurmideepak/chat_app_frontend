084bf625d67b0e9d90aaca0cf84bfdfb00538cb0
Dipak Kurmi <kurmideepak7@gmail.com>
2025-06-01 03:57:42 +0530

    new updated ON mongo url for atlas3

--- src/config/AxiosHelper.js
+++ src/config/AxiosHelper.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+// Use the deployed backend URL for production
 export const baseURL = "https://chatappbackend-production.up.railway.app";
 
 export const httpClient = axios.create({
